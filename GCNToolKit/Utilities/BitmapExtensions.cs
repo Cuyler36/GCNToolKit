49632127fbd5001ec1572f0315bc276635277347
Cuyler36 <Cuyler36@users.noreply.github.com>
2019-11-19 01:16:55 -0500

    Move to .NET Core 3.0
    This also requires my BinaryX class to run now.

--- GCNToolKit/Utilities/BitmapExtensions.cs
+++ GCNToolKit/Utilities/BitmapExtensions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Drawing;
-using System.Runtime.InteropServices;
-using System.Windows;
-using System.Windows.Interop;
+﻿using System.Drawing;
 
 namespace GCNToolKit.Utilities
 {
